fcf6e901fabda43ea42e9736b8b33510c63a66cb
ibiann <trungpham152000@gmail.com>
2022-04-22 11:33:55 +0700

    10% of layers

---
+++ mers/src/Components/AppBar/Appbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Appbar.scss';
+
+function AppBar() {
+  return (
+    <nav className="navbar-app">App bar</nav>
+  )
+}
+
+export default AppBar
---
+++ mers/src/Components/AppBoard/Appboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Appboard.scss'
+
+function BoardBar() {
+  return (
+    <nav className="navbar-board">Board bar</nav>
+  )
+}
+
+export default BoardBar
